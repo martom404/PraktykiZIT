f725dc80d5824ac5fce944d9910d1977767bda14
Marcin Tomaszowski <marcin.tomaszowski2@gmail.com>
2025-07-09 09:37:48 +0200

    16th lesson

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,9 +9,9 @@ class Job extends Model
 {
     use HasFactory;
     protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary'];
-
-    public function employer() 
+    //protected $fillable = ['title', 'salary', 'employer_id'];
+    protected $guarded = [];
+    public function employer()
     {
         return $this->belongsTo(Employer::class);
     }
